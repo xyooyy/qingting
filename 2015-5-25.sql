e4e3e4763c2ee9f14dc83b9a48b118c14b437c04
tianlitao <tianlitao3399@gmail.com>
2015-05-29 09:25:31 +0800

    tialnitao- modify sql and order desc

--- 2015-5-25.sql
+++ 2015-5-25.sql
@@ -1,5 +1,5 @@
 alter table tongji add column ip varchar(30);
 alter table tongji add column area varchar(30);
-alter table tongji add column basic_inf varchar(255);
+alter table tongji add column basic_info varchar(255);
 
 
--- application/models/active_model.php
+++ application/models/active_model.php
@@ -33,7 +33,7 @@ class Active_model extends CI_Model
         $end = $_GET['end'] ? $_GET['end'] : 10;
         //分页开始值
         $start = $_GET['p'] ? ($_GET['p']) : 0;
-        $sql = "select * from " . $this->table . $where . " order by " . $order . " limit " . $start . "," . $end;
+        $sql = "select * from " . $this->table . $where . " order by " . $order  . " desc limit " . $start . "," . $end;
 
         $query = $this->db->query($sql);
         $row = $query->result_array();
