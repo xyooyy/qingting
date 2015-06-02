67266780b90da556af5c4f0fe4f8e39c456876c0
tianlitao <tianlitao3399@gmail.com>
2015-06-02 10:03:20 +0800

    tianlitao- set percent

--- application/views/active/begame4.php
+++ application/views/active/begame4.php
@@ -159,7 +159,7 @@
                                         <td><?php echo $val['prize']['p_name'] ?></td>
                                         <td><?php echo $val['prize']['p_count'] ?></td>
                                         <td><?php echo $val['prize']['p_size'] ?></td>
-                                        <td><?php echo $val['prize']['p_size'] ?></td>
+                                        <td><?php echo $val['prize']['p_size'] . '%' ?></td>
                                         </tr>
                                         </tbody>
                                     </table>
