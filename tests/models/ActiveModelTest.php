2f3f24cea12f4f4b80338cb48bc7d574539d8ead
tianlitao <tianlitao3399@gmail.com>
2015-06-05 18:02:39 +0800

    tianlitao delete phone_carrier_model conflict resolution

--- tests/models/ActiveModelTest.php
+++ tests/models/ActiveModelTest.php
@@ -16,54 +16,58 @@ class ActiveModelTest extends CIUnit_TestCase
         $this->CI->load->model('active_model');
         $this->_pcm = $this->CI->active_model;
         $this->session_user_id = 10;
-        $session = array('userid'=>$this->session_user_id);
+        $session = array('userid' => $this->session_user_id);
         $this->_pcm->session->set_userdata($session);
 
-        $this->data= array(
-                'title' => '吃饭了',
-                'type' => 1,
-                'fenxiangt'=>'饭好了#score#吃不吃吧',
-                'fenxiangc'=>'听见没',
-                'starttime'=>'1430390820',
-                'endtime'=>'1430995620',
-                'gid'=>3,
-                'ischou'=>1,
-                'prize_t'=>'qt',
-                'prize_c'=>3,
-                'prize_c1'=>100,
-                'prize_s'=>0,
-                'userid'=>$this->session_user_id
-            );
+        $this->data = array(
+            'title' => '吃饭了',
+            'type' => 1,
+            'fenxiangt' => '饭好了#score#吃不吃吧',
+            'fenxiangc' => '听见没',
+            'starttime' => '1430390820',
+            'endtime' => '1430995620',
+            'gid' => 3,
+            'ischou' => 1,
+            'prize_t' => 'qt',
+            'prize_c' => 3,
+            'prize_c1' => 100,
+            'prize_s' => 0,
+            'userid' => $this->session_user_id
+        );
 
     }
 
-    public function tearDown(){
+    public function tearDown()
+    {
         $clear_data_sql = "truncate table " . $this->_pcm->table;
         $this->_pcm->db->query($clear_data_sql);
     }
 
-    public function test_where(){
-        $expend = array('id > 0', 'userid='. $this->session_user_id);
+    public function test_where()
+    {
+        $expend = array('id > 0', 'userid=' . $this->session_user_id);
         $get_data = null;
 
-        $get_data = $this->_pcm ->where();
+        $get_data = $this->_pcm->where();
 
         $this->assertEquals($expend, $get_data);
     }
 
-    public function test_all_before_insert_data(){
+    public function test_all_before_insert_data()
+    {
         $expend_all_count = 0;
         $get_data = null;
-        $order='id';
-        $start=0;
-        $end=10;
+        $order = 'id';
+        $start = 0;
+        $end = 10;
 
-        $get_data = $this->_pcm->all($order, $start,$end);
+        $get_data = $this->_pcm->all($order, $start, $end);
 
         $this->assertEquals($expend_all_count, count($get_data));
     }
 
-    public function test_insert(){
+    public function test_insert()
+    {
         $expend_active_id = 1;
         $get_data;
 
@@ -72,29 +76,29 @@ class ActiveModelTest extends CIUnit_TestCase
         $this->assertEquals($expend_active_id, $get_data);
     }
 
-    public function test_all_after_insert_data(){
-            $expend_all_count = 1;
-            $get_data = null;
-            $order='id';
-            $start=0;
-            $end=10;
-
-            $this->_pcm->ins($this->data);
-            $get_data = $this->_pcm->all($order, $start,$end);
-
-            $this->assertEquals($expend_all_count, count($get_data));
-    }
-
-<<<<<<< HEAD
-}
-=======
-    public function test_del(){
-        $expend_all_count = 0;
-        $get_data=null;
+    public function test_all_after_insert_data()
+    {
+        $expend_all_count = 1;
+        $get_data = null;
         $order = 'id';
         $start = 0;
         $end = 10;
-        $this->_pcm -> ins($this->data);
+
+        $this->_pcm->ins($this->data);
+        $get_data = $this->_pcm->all($order, $start, $end);
+
+        $this->assertEquals($expend_all_count, count($get_data));
+    }
+
+
+    public function test_del()
+    {
+        $expend_all_count = 0;
+        $get_data = null;
+        $order = 'id';
+        $start = 0;
+        $end = 10;
+        $this->_pcm->ins($this->data);
         $exist_active_list = $this->_pcm->all($order, $start, $end);
         $this->assertEquals(1, count($exist_active_list));
 
@@ -104,4 +108,3 @@ class ActiveModelTest extends CIUnit_TestCase
     }
 
 }
->>>>>>> a0296880798efaf99d6a92f043f1e36dd8243a80
