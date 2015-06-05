f0603719f124630d7baace3a3e1dc890f550e0c1
fortunezhang <zhangfortune527@gmail.com>
2015-06-05 21:12:31 +0800

    fortune- test active_model -> info

--- tests/models/ActiveModelTest.php
+++ tests/models/ActiveModelTest.php
@@ -19,21 +19,38 @@ class ActiveModelTest extends CIUnit_TestCase
         $session = array('userid' => $this->session_user_id);
         $this->_pcm->session->set_userdata($session);
 
-        $this->data = array(
-            'title' => '吃饭了',
-            'type' => 1,
-            'fenxiangt' => '饭好了#score#吃不吃吧',
-            'fenxiangc' => '听见没',
-            'starttime' => '1430390820',
-            'endtime' => '1430995620',
-            'gid' => 3,
-            'ischou' => 1,
-            'prize_t' => 'qt',
-            'prize_c' => 3,
-            'prize_c1' => 100,
-            'prize_s' => 0,
-            'userid' => $this->session_user_id
-        );
+        $this->data= array(
+                'title' => '吃饭了',
+                'info' => '你妈妈叫你回家吃饭',
+                'href' => 'zhangfortune.com',
+                'type' => 1,
+                'icon'=>'zhangfortune.com/public/icon.png',
+                'fenxiangt'=>'饭好了#score#吃不吃吧',
+                'fenxiangc'=>'听见没',
+                'fenxiangi'=>'./upload/active/20150430-14303909187754.jpg',
+                'starttime'=>'1430390820',
+                'endtime'=>'1430995620',
+                'html_start'=>'hello start',
+                'html_start1'=>'hello start1',
+                'html_end' => 'hello end',
+                'html_end1' => 'hello end1',
+                'html_fenxiang'=> 'hello html_fenxiang',
+                'html_fenxiang1'=> ' hello html_fenxiang1',
+                'html_prize'=>'hello html_prize',
+                'html_prize1'=>'hello html_prize1',
+                'html_prize_not_win' => 'hello html_prize_not_win',
+                'html_prize_not_win1' => 'hello html_prize_not_win1',
+                'html_prize_delete_chance' => 'hello html_prize_delete_chance',
+                'html_prize_delete_chance1' => 'hello html_prize_delete_chance1',
+                'erweima'=> 'active/20150430-14303909381220.png',
+                'gid'=>3,
+                'ischou'=>1,
+                'prize_t'=>'qt',
+                'prize_c'=>3,
+                'prize_c1'=>100,
+                'prize_s'=>0,
+                'userid'=>$this->session_user_id
+            );
 
     }
 
@@ -69,7 +86,7 @@ class ActiveModelTest extends CIUnit_TestCase
     public function test_insert()
     {
         $expend_active_id = 1;
-        $get_data;
+        $get_data = null;
 
         $get_data = $this->_pcm->ins($this->data);
 
@@ -106,5 +123,29 @@ class ActiveModelTest extends CIUnit_TestCase
         $get_data = $this->_pcm->all($order, $start, $end);
         $this->assertEquals($expend_all_count, count($get_data));
     }
+
+    /**
+        *@dataProvider test_info_provider
+    */
+    public function test_info($tfrom, $tvalue, $expend){
+        
+        $get_data=null;
+        $order = 'id';
+        $start = 0;
+        $end = 10;
+        $this->_pcm -> ins($this->data);
+        
+        $get_data = $this->_pcm->info($tfrom, $tvalue);
+
+        $this->assertEquals($expend, $get_data);
+    }
+    public function test_info_provider(){
+        return array(
+                array('title', $this->data['title'], $this->data),
+                array('gid', $this->data['gid'], $this->data),
+            );
+    }
+
+
 
 }
